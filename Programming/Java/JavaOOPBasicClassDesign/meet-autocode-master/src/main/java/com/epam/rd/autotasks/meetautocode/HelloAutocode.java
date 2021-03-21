afd2dbaa853481a67789e7a4d652916ff64c6c53
GG <72414103+spbgzh@users.noreply.github.com>
2021-03-21 08:31:43 +0300

    Delete Programming/Java/JavaOOPBasicClassDesign directory

--- Programming/Java/JavaOOPBasicClassDesign/meet-autocode-master/src/main/java/com/epam/rd/autotasks/meetautocode/HelloAutocode.java
+++
@@ -1,8 +0,0 @@
-package com.epam.rd.autotasks.meetautocode;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        // Write a program, printing "Hello, Autocode!"
-        System.out.println("Hello, Autocode!");
-    }
-}
