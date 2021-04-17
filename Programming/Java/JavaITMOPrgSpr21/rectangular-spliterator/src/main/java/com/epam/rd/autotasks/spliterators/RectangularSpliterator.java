77060043592d079a536be22e8417f5f0e6c23f60
spbgzh <zjjhgzh@gmail.com>
2021-04-17 18:24:11 +0300

    rewrite spliterators

--- Programming/Java/JavaITMOPrgSpr21/rectangular-spliterator/src/main/java/com/epam/rd/autotasks/spliterators/RectangularSpliterator.java
+++ Programming/Java/JavaITMOPrgSpr21/rectangular-spliterator/src/main/java/com/epam/rd/autotasks/spliterators/RectangularSpliterator.java
@@ -1,20 +1,86 @@
 package com.epam.rd.autotasks.spliterators;
 
+import java.util.Arrays;
 import java.util.Spliterator;
 import java.util.function.IntConsumer;
 
-public interface RectangularSpliterator extends Spliterator.OfInt {
+public class RectangularSpliterator implements Spliterator.OfInt {
+
+    int[][] content;
+    int[] itercon; // int curnumba;
+
+    public RectangularSpliterator(int[][] array) {
+        this.content = array;
+        // this.curnumba = -1;
+        this.itercon = Arrays.stream(array).flatMapToInt(Arrays::stream).toArray();
+    }
+
+    static RectangularSpliterator of(int[][] array) {
+        return new RectangularSpliterator(array);
+    }
 
     @Override
-    RectangularSpliterator trySplit();
+    public RectangularSpliterator trySplit() {
+        if (this.content == null)
+            return null;
+        if (this.content.length * this.content[0].length <= 1)
+            return null;
+        int[][] fhalf = null, shalf = null;
+        if (content[0].length <= content.length) {
+            fhalf = new int[this.content.length / 2][this.content[0].length];
+            for (int i = 0; i < this.content.length / 2; i++) {
+                for (int j = 0; j < this.content[0].length; j++)
+                    fhalf[i][j] = content[i][j];
+            }
+            shalf = new int[(this.content.length + 1) / 2][this.content[0].length];
+            for (int i = this.content.length / 2; i < this.content.length; i++) {
+                for (int j = 0; j < this.content[0].length; j++)
+                    shalf[i - this.content.length / 2][j] = content[i][j];
+            }
+        } else {
+            fhalf = new int[this.content.length][this.content[0].length / 2];
+            for (int i = 0; i < this.content.length; i++) {
+                for (int j = 0; j < this.content[0].length / 2; j++)
+                    fhalf[i][j] = content[i][j];
+            }
+            shalf = new int[this.content.length][(this.content[0].length + 1) / 2];
+            for (int i = 0; i < this.content.length; i++) {
+                for (int j = this.content[0].length / 2; j < this.content[0].length; j++)
+                    shalf[i][j - this.content[0].length / 2] = content[i][j];
+            }
+        }
+        this.content = shalf;
+        RectangularSpliterator newborn = new RectangularSpliterator(fhalf);
+        // this.curnumba = -1;
+        this.itercon = Arrays.stream(content).flatMapToInt(Arrays::stream).toArray();
+        return newborn;
+    }
 
     @Override
-    boolean tryAdvance(IntConsumer action);
+    public boolean tryAdvance(IntConsumer action) {
+        // if(curnumba + 1 < itercon.length) {
+        if (itercon.length > 0) {
+            // curnumba++;
+            action.accept(itercon[0]);
+            int[] tmp = new int[itercon.length - 1];
+            for (int y = 0; y < itercon.length - 1; y++)
+                tmp[y] = itercon[y + 1];
+            itercon = tmp;
+            this.content = new int[1][];
+            content[0] = itercon;
+            return true;
+        }
+        return false;
+    }
 
     @Override
-    long estimateSize();
+    public long estimateSize() {
+        // return this.content == null ? 0:this.content.length * this.content[0].length;
+        return this.itercon.length;
+    }
 
-    static RectangularSpliterator of(int[][] array){
-        throw new UnsupportedOperationException();
+    @Override
+    public int characteristics() {
+        return CONCURRENT + NONNULL;
     }
 }
